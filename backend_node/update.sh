800ed57fb56cd330f41b7504174d6217e7f4ddb3
Alex Sim <alexandersim@protonmail.com>
2025-06-23 14:21:37 +0100

    separates updating portainer to different file

---
+++ backend_node/update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ 
+# Update portainer
+docker stop portainer
+docker rm portainer
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
